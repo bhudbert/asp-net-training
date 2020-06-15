dd037411bc0eca842401b9cf596fe838a5ecc9b9
Bruno HUDBERT <dev@bruno.hudbert.fr>
2020-06-16 00:24:27 +0200

    first commit

---
+++ Module_2_TP_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_2_TP_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Module_2_TP_1/forme.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_2_TP_1
+{
+    interface forme
+    {
+    }
+}
